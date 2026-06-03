62434b83ee7e143a4062b9389ab9b399e5f7f86e
Deepak <darkdeepak001@gmail.com>
2026-06-03 10:23:57 +0530

    Refactor task management in script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-<script>
-    var u = document.getElementById("task")
     var input = document.getElementById("input")
+    var t = document.getElementById("task")
+    var total = document.getElementById("total")
+    var completed = document.getElementById("completed")
+    var totalcount = 0
+    var completedcount = 0
     function add()
     {
         var listitem=document.createElement("li")
         
-        listitem.innerHTML=input.value + "<button onclick=deleteItem(event)>Delete</button>"
+        listitem.innerHTML=input.value + "<button onclick='completetask(event)'>Completed</button>" + "<button onclick='deleteItem(event)'>Delete</button>"
         
-        u.append(listitem)
+        t.append(listitem)
+        input.value=""
+        totalcount++
+        total.textContent=totalcount
         
+    }
+    function completetask(event)
+    {
+        var t = event.target.parentElement
+        t.style.textDecoration = "line-through"
+        completedcount++
+        completed.textContent=completedcount
+        event.target.disabled = true
+        event.target.textContent = "Done-->"
     }
     function deleteItem(event)
     {
         event.target.parentElement.remove()
     }
-</script>
